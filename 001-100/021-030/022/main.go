d990f492170a858222b9739ad5c4334f6f1e6523
toma <toma@Vera.lan>
2019-07-24 22:22:10 +0100

    probs 022 && 023

---
+++ 001-100/021-030/022/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/TomasCruz/projecteuler"
+)
+
+/*
+Problem 22; Names scores
+
+Using names.txt, a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order.
+Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to
+obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is
+the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+*/
+
+type nameType []string
+
+func (a nameType) Len() int           { return len(a) }
+func (a nameType) Less(i, j int) bool { return a[i] < a[j] }
+func (a nameType) Swap(i, j int)      { t := a[i]; a[i] = a[j]; a[j] = t }
+
+func main() {
+	projecteuler.Timed(calc)
+}
+
+func calc(args ...interface{}) (err error) {
+	var textNumbers []string
+	if textNumbers, err = projecteuler.FileToStrings("p022_names.txt"); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	names := strings.Split(textNumbers[0], ",")
+	for i := 0; i < len(names); i++ {
+		names[i] = names[i][1 : len(names[i])-1]
+	}
+	sort.Sort(nameType(names))
+
+	totalSum := 0
+	for i := 0; i < len(names); i++ {
+		totalSum += (i + 1) * alphaSum(names[i])
+	}
+
+	fmt.Println(totalSum)
+	return
+}
+
+func alphaSum(s string) (sum int) {
+	for _, c := range s {
+		sum += int(c - 'A' + 1)
+	}
+	return
+}
---
+++ 001-100/021-030/023/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/TomasCruz/projecteuler"
+)
+
+/*
+Problem 23; Non-abundant sums
+
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number.
+For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant
+if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two
+abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the
+sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known
+that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+*/
+
+func main() {
+	var limit int
+
+	if len(os.Args) > 1 {
+		limit64, err := strconv.ParseInt(os.Args[1], 10, 64)
+		if err != nil {
+			log.Fatal("bad argument")
+		}
+
+		limit = int(limit64)
+	} else {
+		limit = 28124
+	}
+
+	projecteuler.Timed(calc, limit)
+}
+
+func calc(args ...interface{}) (err error) {
+	limit := args[0].(int)
+
+	// for limit == 25, sum = 1..23 = 23*12 = 230+46 = 276
+	primes := projecteuler.Primes(limit, nil)
+	properDivisors := make([]int, limit)
+
+	for i := 4; i < limit; i++ {
+		var currFactors map[int]int
+		if currFactors, err = projecteuler.Factorize(i, primes); err != nil {
+			return
+		}
+
+		properDivisors[i] = divisorSum(currFactors) - i
+	}
+
+	abundant := make([]int, 0)
+	abundantSet := make(map[int]struct{})
+	for i := 4; i < limit; i++ {
+		if properDivisors[i] > i {
+			abundant = append(abundant, i)
+			abundantSet[i] = struct{}{}
+		}
+	}
+
+	nonAbundantSum := make([]int, 0)
+	for i := 1; i < limit; i++ {
+		if isNonAbundantSum(i, abundant, abundantSet) {
+			nonAbundantSum = append(nonAbundantSum, i)
+		}
+	}
+
+	totalSum := 0
+	for _, n := range nonAbundantSum {
+		totalSum += n
+	}
+
+	fmt.Println(totalSum)
+	return
+}
+
+func divisorSum(factors map[int]int) (sum int) {
+	divisors := findDivisors(factors)
+
+	for i := 0; i < len(divisors); i++ {
+		sum += divisors[i]
+	}
+
+	return
+}
+
+func findDivisors(factors map[int]int) (divisors []int) {
+	var firstK, firstV int
+
+	otherFactors := make(map[int]int)
+	for k, v := range factors {
+		if firstK == 0 {
+			firstK = k
+			firstV = v
+		} else {
+			otherFactors[k] = v
+		}
+	}
+
+	powers := make([]int, firstV+1)
+	powers[0] = 1
+	for i := 0; i < firstV; i++ {
+		powers[i+1] = powers[i] * firstK
+	}
+
+	if len(otherFactors) == 0 {
+		return powers
+	}
+
+	otherDivisors := findDivisors(otherFactors)
+	otherDivisorCount := len(otherDivisors)
+	divisors = make([]int, (firstV+1)*otherDivisorCount)
+
+	for i := 0; i <= firstV; i++ {
+		for j := 0; j < otherDivisorCount; j++ {
+			divisors[i*otherDivisorCount+j] = powers[i] * otherDivisors[j]
+		}
+	}
+
+	return
+}
+
+func isNonAbundantSum(x int, abundant []int, abundantSet map[int]struct{}) bool {
+	for i := 0; i < len(abundant) && abundant[i] < x; i++ {
+		rest := x - abundant[i]
+		if rest < abundant[i] {
+			break
+		}
+
+		if _, ok := abundantSet[rest]; ok {
+			return false
+		}
+	}
+
+	return true
+}
