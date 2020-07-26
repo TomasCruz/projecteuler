494791c82b036c1dfb3b642f806a00fefbdca626
toma <toma@Vera.lan>
2020-07-26 22:46:35 +0100

    remove the number

--- 001-100/061-070/062/main.go
+++ 001-100/061-070/062/main.go
@@ -16,7 +16,6 @@ The cube, 41063625 (345^3), can be permuted to produce two other cubes: 56623104
 In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.
 
 Find the smallest cube for which exactly five permutations of its digits are cube.
-127035954683
 */
 
 func main() {
