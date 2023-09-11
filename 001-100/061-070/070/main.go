2900618759af8b8d79c146d4da4e97214207adfe
Toma Krstic <toma.krstic.76@gmail.com>
2023-09-11 13:14:16 +0100

    optimized 070

--- 001-100/061-070/070/main.go
+++ 001-100/061-070/070/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 
 	"github.com/TomasCruz/projecteuler"
@@ -40,40 +41,26 @@ func main() {
 
 func calc(args ...interface{}) (result string, err error) {
 	limit := args[0].(int)
+	primes := projecteuler.Primes(limit, nil)
 
-	m := map[int]map[int]struct{}{} // smallest perm -> set of perms
-
-	checked := map[int]struct{}{}
-	for n := 2; n < limit; n++ {
-		if _, present := checked[n]; present {
-			continue
-		}
-
-		smallest, digitPerms := permMap(n)
-		if _, present := m[smallest]; !present {
-			m[smallest] = digitPerms
-			for p := range digitPerms {
-				checked[p] = struct{}{}
-			}
-		}
+	minPrimeIndex := 20
+	for ; primes[minPrimeIndex] < 1000; minPrimeIndex++ {
 	}
 
+	lenPrimes := len(primes)
 	minX := 0
 	minRatio := math.MaxFloat64
-	primes := projecteuler.Primes(limit, nil)
-	for smallest, perms := range m {
-		if len(perms) == 1 {
-			continue
-		}
 
-		for x := range perms {
-			if x == smallest {
-				continue
+	for i := minPrimeIndex; i < lenPrimes; i++ {
+		for j := i + 1; j < lenPrimes; j++ {
+			x := primes[i] * primes[j]
+			if x >= limit {
+				break
 			}
 
-			t := projecteuler.Totient(x, primes)
-			if _, present := perms[t]; present {
-				ratio := float64(x) / float64(t)
+			tot := totient(i, j, primes)
+			if permutation(x, tot) {
+				ratio := float64(x) / float64(tot)
 				if ratio < minRatio {
 					minX = x
 					minRatio = ratio
@@ -86,96 +73,37 @@ func calc(args ...interface{}) (result string, err error) {
 	return
 }
 
-type digitalNumber struct {
-	x      int
-	digits string
+func totient(i, j int, primes []int) int {
+	return (primes[i] - 1) * (primes[j] - 1)
 }
 
-func newDigitalNumber(pdn projecteuler.DigitalNumber) digitalNumber {
-	digs := pdn.Digits()
-	l := len(digs)
-	digits := make([]byte, 0, l)
-	for i := 0; i < l; i++ {
-		digits = append(digits, digs[i]+'0')
+func permutation(a, b int) bool {
+	aDigits := []int{}
+	for a > 0 {
+		aDigits = append(aDigits, a%10)
+		a /= 10
+	}
+	aLen := len(aDigits)
+
+	bDigits := []int{}
+	for b > 0 {
+		bDigits = append(bDigits, b%10)
+		b /= 10
+	}
+	bLen := len(bDigits)
+
+	if aLen != bLen {
+		return false
 	}
 
-	// reverse it
-	for i, j := 0, l-1; i < j; i, j = i+1, j-1 {
-		digits[i], digits[j] = digits[j], digits[i]
-	}
+	sort.Ints(aDigits)
+	sort.Ints(bDigits)
 
-	return digitalNumber{
-		x:      pdn.X(),
-		digits: string(digits),
-	}
-}
-
-func newDigitalNumberFromString(s string) digitalNumber {
-	x := 0
-	pow := 1
-	l := len(s)
-	for i := l; i > 0; i-- {
-		x += pow * int(s[i-1]-'0')
-		pow *= 10
-	}
-
-	return digitalNumber{
-		x:      x,
-		digits: s,
-	}
-}
-
-func permMap(x int) (smallest int, digitPerms map[int]struct{}) {
-	peDigitalX := projecteuler.NewDigitalNumber(x)
-	digitalX := newDigitalNumber(peDigitalX)
-	l := len(digitalX.digits)
-
-	perms := projecteuler.Permutations(byte(l), nil)
-	smallest = math.MaxInt
-	digitPerms = map[int]struct{}{}
-	for _, p := range perms {
-		if digitalX.digits[p[0]] == '0' {
-			continue
-		}
-
-		currDigits := make([]byte, l)
-		for i := 0; i < l; i++ {
-			currDigits[i] = digitalX.digits[p[i]]
-		}
-
-		dn := newDigitalNumberFromString(string(currDigits))
-		if dn.x < smallest {
-			smallest = dn.x
-		}
-		digitPerms[dn.x] = struct{}{}
-	}
-
-	return
-}
-
-func phi(n int, primes []int) int {
-	// Initialize result as n
-	result := float64(n)
-
-	// Consider all prime factors of n and for every prime factor p, multiply result with (1 - 1/p)
-	for i := 0; primes[i]*primes[i] <= n; i++ {
-		// Check if it is a prime factor.
-		if n%primes[i] == 0 {
-			// update n and result
-			for n%primes[i] == 0 {
-				n /= primes[i]
-			}
-			result *= (1.0 - (1.0 / float64(primes[i])))
+	for i := 0; i < aLen; i++ {
+		if aDigits[i] != bDigits[i] {
+			return false
 		}
 	}
 
-	// If n has a prime factor greater than sqrt(n), there can be at-most one such prime factor
-	if n > 1 {
-		result -= result / float64(n)
-	}
-
-	//Since in the set {1,2,....,n-1}, all numbers are relatively prime with n
-	//if n is a prime number
-
-	return int(result)
+	return true
 }
