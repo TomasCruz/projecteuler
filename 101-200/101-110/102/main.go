4de158b304df38687adb90eda83c8466d93011f0
Toma Krstic <toma.krstic.76@gmail.com>
2026-01-17 20:03:43 +0100

    102

---
+++ 101-200/101-110/102/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/TomasCruz/projecteuler"
+)
+
+/*
+Problem 102; Triangle Containment
+Three distinct points are plotted at random on a Cartesian plane, for which -1000 <= x, y <= 1000, such that a triangle is formed.
+
+Consider the following two triangles:
+A(-340,495), B(-153,-910), C(835,-947)
+X(-175,41), Y(-421,-714), Z(574,-645)
+
+It can be verified that triangle ABC contains the origin, whereas triangle XYZ does not.
+Using triangles.txt(right click and 'Save Link/Target As...'), a 27K text file containing the co-ordinates of one thousand "random" triangles,
+find the number of triangles for which the interior contains the origin.
+
+NOTE: The first two examples in the file represent the triangles in the example given above.
+*/
+
+func main() {
+	var limit int
+
+	if len(os.Args) > 1 {
+		limit64, err := strconv.ParseInt(os.Args[1], 10, 64)
+		if err != nil {
+			log.Fatal("bad argument")
+		}
+
+		limit = int(limit64)
+	} else {
+		limit = 1
+	}
+
+	projecteuler.Timed(calc, limit)
+}
+
+func calc(args ...interface{}) (result string, err error) {
+	limit := args[0].(int)
+
+	var fileName string
+	if limit == 2 {
+		fileName = "example.txt"
+	} else {
+		fileName = "0102_triangles.txt"
+	}
+
+	var rowStrings []string
+	if rowStrings, err = projecteuler.FileToStrings(fileName); err != nil {
+		return
+	}
+
+	sum := 0
+	for _, s := range rowStrings {
+		coordsStr := strings.Split(s, ",")
+		coords := make([]int, 6)
+
+		for i := 0; i < 6; i++ {
+			coords[i], err = strconv.Atoi(coordsStr[i])
+			if err != nil {
+				return
+			}
+		}
+
+		a := point{x: coords[0], y: coords[1]}
+		b := point{x: coords[2], y: coords[3]}
+		c := point{x: coords[4], y: coords[5]}
+
+		if triangleContainsOrigin(a, b, c) {
+			sum++
+		}
+	}
+
+	result = strconv.Itoa(sum)
+	return
+}
---
+++ 101-200/101-110/102/point.go
@@ -0,0 +1,33 @@
+package main
+
+type point struct {
+	x, y int
+}
+
+func dotProduct(p1, p2 point) int {
+	return p1.x*p2.x + p1.y*p2.y
+}
+
+func triangleContainsOrigin(a, b, c point) bool {
+	// using barycentric coordinates
+	v0 := point{x: c.x - a.x, y: c.y - a.y}
+	v1 := point{x: b.x - a.x, y: b.y - a.y}
+	v2 := point{x: -a.x, y: -a.y}
+
+	d00 := dotProduct(v0, v0)
+	d01 := dotProduct(v0, v1)
+	d11 := dotProduct(v1, v1)
+	d02 := dotProduct(v0, v2)
+	d12 := dotProduct(v1, v2)
+
+	denom := d00*d11 - d01*d01
+	l2 := float64(d11*d02-d01*d12) / float64(denom)
+	l3 := float64(d00*d12-d01*d02) / float64(denom)
+	l1 := 1 - l2 - l3
+
+	if l1 >= 0.0 && l1 <= 1.0 && l2 >= 0.0 && l2 <= 1.0 && l3 >= 0.0 && l3 <= 1.0 {
+		return true
+	}
+
+	return false
+}
