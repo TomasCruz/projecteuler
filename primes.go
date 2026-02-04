579b78b32dd51c28a9aa325fa4719c6ecaac09d3
Toma Krstic <toma.krstic.76@gmail.com>
2026-02-04 15:23:46 +0100

    146 2

--- primes.go
+++ primes.go
@@ -61,3 +61,70 @@ func IsPrime(x int64) bool {
 
 	return true
 }
+
+type Bitset []uint64
+
+// NewBitset construct new Bitset, all the bits set to 0 (false)
+func NewBitset(n uint64) Bitset {
+	return make(Bitset, (n+63)/64)
+}
+
+func (b Bitset) Get(index uint64) bool {
+	pos := index / 64
+	j := index % 64
+	return (b[pos] & (uint64(1) << j)) != 0
+}
+
+func (b Bitset) Set(index uint64, value bool) {
+	pos := index / 64
+	j := index % 64
+
+	if value {
+		b[pos] |= uint64(1) << j
+	} else {
+		m := uint64(math.MaxUint64)
+		m -= uint64(1) << j
+		b[pos] &= m
+	}
+}
+
+// PrimesEratosthenes calculates and returns slice of primes smaller than limit, or until f returns true
+func PrimesEratosthenes(limit uint64, f func(...interface{}) bool, args ...interface{}) (primes []uint64) {
+	bs := NewBitset(limit + 1)
+
+	for i := uint64(2); i < limit; i++ {
+		if bs.Get(i) {
+			continue
+		}
+
+		for j := uint64(2 * i); j < limit; j += i {
+			bs.Set(j, true)
+		}
+	}
+
+	var ret []uint64
+	if limit <= uint64(100000) {
+		ret = make([]uint64, 0, limit/2)
+	} else {
+		ret = make([]uint64, 0, limit/10)
+	}
+
+	ret = append(ret, uint64(2))
+
+	for i := uint64(3); i <= limit; i += 2 {
+		if bs.Get(i) {
+			continue
+		}
+
+		ret = append(ret, i)
+		if f != nil {
+			args = append(args, ret)
+			if f(args...) {
+				break
+			}
+			args = args[:len(args)-1]
+		}
+	}
+
+	return ret
+}
